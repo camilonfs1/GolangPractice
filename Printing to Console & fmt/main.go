9d8428f9724abad5d97c8367e16829899852a016
camilonfs1 <camilonfs1@gmail.com>
2022-02-21 20:08:27 -0500

    first commit

--- Printing to Console & fmt/main.go
+++ Printing to Console & fmt/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Printf("%T \n", 10)
+
 	fmt.Printf("Hello %T %v", 10, 10)
 
 	fmt.Printf("\n %%", 0.10)
+
+	fmt.Printf("\n Number : %9q", "Tim")
+
+	var out string = fmt.Sprintf("Number: %07d is cool", 45)
+
+	fmt.Println("\n", out)
 
 }
